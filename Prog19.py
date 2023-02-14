3ddf4c14407454229fc07176c146194105caa18a
Anastasia <nastasy.kozh@gmail.com>
2023-02-14 05:32:30 +0300

    Adding module 4 (19-22)

---
+++ Prog19.py
@@ -0,0 +1,8 @@
+txt = 'Напишите абв пабврограмму программу, удаляющую из этого незабвенного текста все абвслова слова, содерабващие содержащие последовательность букв а, б и в "абв"'
+
+def remove(txt):
+    txt = list(filter(lambda x: 'абв' not in x, txt.split()))
+    return ' '.join(txt)
+
+txt = remove(txt)
+print(txt)
